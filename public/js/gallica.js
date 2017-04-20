8fc22b22f822cebe3c7100e00924f057447e0c3d
amalricBzh <amaury@pinsonnais.org>
2017-04-20 08:54:05 +0200

    Guillemets doubles + percetToRgb défini avant utilisation

--- public/js/gallica.js
+++ public/js/gallica.js
@@ -1,6 +1,19 @@
 var CfgTimeoutTries = 3;
 var CfgTimeoutOccured = 0;
 
+function percentToRgb(percent) {
+	var red = 0;
+	var green = 0;
+	if (percent <= 50) {
+		red = 255;
+		green = Math.floor(percent * 255 / 50);
+	} else {
+		red = Math.floor((100 - percent) * 255 / 50);
+		green = 255;
+	}
+	return "rgb(" + red + ", " + green + ", 0)";
+}
+
 function waitAndDownloadPage(duration, documentId) {
 	duration = typeof duration !== "undefined" ? duration : 0;
 	// On attend entre 0.5 et 1 secondes, plus éventuellement un autre délai passé en paramètre
@@ -169,10 +182,10 @@ $(document).ready(function () {
 		},
 		change: function () {
 			var percent = Math.floor(parseInt(progressbar.progressbar("value")) * 100 / max);
-			progressLabel.text(progressbar.progressbar("value") + "/" + max + ' (' + percent + '%)');
+			progressLabel.text(progressbar.progressbar("value") + "/" + max + " (" + percent + "%)");
 			var red = Math.floor((100 - percent) * 255 / 100);
 			var green = Math.floor(percent * 255 / 100);
-			$('.ui-progressbar-value').css(
+			$(".ui-progressbar-value").css(
 				"background-color", percentToRgb(percent)
 			);
 		},
@@ -182,15 +195,4 @@ $(document).ready(function () {
 	});
 });
 
-function percentToRgb(percent) {
-	var red = 0;
-	var green = 0;
-	if (percent <= 50) {
-		red = 255;
-		green = Math.floor(percent * 255 / 50);
-	} else {
-		red = Math.floor((100 - percent) * 255 / 50);
-		green = 255;
-	}
-	return 'rgb(' + red + ', ' + green + ', 0)';
-}
+
