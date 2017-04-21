f47cc469ef4231b82029793afc747a15175c3fb4
amalricBzh <amaury@pinsonnais.org>
2017-04-21 19:36:44 +0200

    Simplification + commentaire

--- src/Service/UnitesService.php
+++ src/Service/UnitesService.php
@@ -8,21 +8,21 @@ class UnitesService
     public function __construct()
     {
     }
-    
+
+    /** Convertit une taille en octet en une chaîne lisible par un humain */
     public function getSize($size = 0)
     {
+	    $txt = '-';
         if ($size >= 1073741824) {
-            $size = number_format($size / 1073741824, 2) . ' Go';
+	        $txt = number_format($size / 1073741824, 2) . ' Go';
         } elseif ($size >= 1048576) {
-            $size = number_format($size / 1048576, 2) . ' Mo';
+	        $txt = number_format($size / 1048576, 2) . ' Mo';
         } elseif ($size >= 1024) {
-            $size = number_format($size / 1024, 2) . ' Ko';
+	        $txt = number_format($size / 1024, 2) . ' Ko';
         } elseif ($size > 0) {
-            $size = $size . ' o';
-        } else {
-            $size = '-';
+	        $txt = $size . ' o';
         }
 
-        return $size;
+        return $txt;
     }
 }
